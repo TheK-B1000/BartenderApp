e2a49019c532563a511f122616cd6f7921143ef8
Kevin-Brandon Joseph Corbett <k-b0514@hotmail.com>
2023-08-27 12:08:51 -0400

    Create DrinksDb

---
+++ BartenderApp/BartenderApp/Models/DrinksDb.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BartenderApp.Models
+{
+    public class DrinksDb : DbContext
+    {
+        public DbSet<Cocktail> Cocktails { get; set;}
+        public DbSet<Order> Orders { get; set;} 
+    }
+}
