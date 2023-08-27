6f492e7b94bffb90a685d513a761ebdb1be6da5d
Kevin-Brandon Joseph Corbett <k-b0514@hotmail.com>
2023-08-27 11:55:03 -0400

    Add Controllers

---
+++ BartenderApp/BartenderApp/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BartenderApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ BartenderApp/BartenderApp/Controllers/QueueController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BartenderApp.Controllers
+{
+    public class QueueController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
