cda5debc6b7aebe093d2aa2d83e309669ff434cf
Kevin-Brandon Joseph Corbett <k-b0514@hotmail.com>
2023-08-27 11:45:15 -0400

    Update Program.cs

--- BartenderApp/BartenderApp/Program.cs
+++ BartenderApp/BartenderApp/Program.cs
@@ -1,6 +1,16 @@
 var builder = WebApplication.CreateBuilder(args);
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
